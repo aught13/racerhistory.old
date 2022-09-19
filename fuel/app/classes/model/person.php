69f7d8bc0e448864dd7b43fa75ce1c186bdbf3a8
Patrick Foltz <historian@racerhistory.com>
2022-09-19 13:36:09 -0500

    add display name field and admin backend to person

--- fuel/app/classes/model/person.php
+++ fuel/app/classes/model/person.php
@@ -1,35 +1,34 @@
 <?php
+
 namespace Model;
 
-class Person extends \Orm\Model
+class Person extends \Orm\Model {
 
-{
     protected static $_properties = [
         'id',
         'first',
         'last',
         'nick',
+        'display',
         'photo',
         'submitted_date',
         'updated_date',
     ];
 
-    public static function validate($factory)
-    {
+    public static function validate($factory) {
         $val = Validation::forge($factory);
         $val->add_field('id', 'Id', 'required|valid_string[numeric]');
         $val->add_field('first', 'First', 'required|max_length[162]');
         $val->add_field('last', 'Last', 'required|max_length[162]');
         $val->add_field('nick', 'Nick', 'max_length[162]');
+        $val->add_field('display, Display', 'max_length[162]');
         $val->add_field('photo', 'Photo', 'max_length[162]');
 
         return $val;
     }
 
     protected static $_table_name = 'persons';
-
     protected static $_primary_key = ['id'];
-
     protected static $_has_many = [
         'player_season_stats' => [
             'key_from' => 'id',
@@ -38,7 +37,6 @@ class Person extends \Orm\Model
             'cascade_save' => true,
             'cascade_delete' => false,
         ],
-
         'person_post' => [
             'key_from' => 'id',
             'model_to' => '\Model\Person\Post',
@@ -47,28 +45,33 @@ class Person extends \Orm\Model
             'cascade_delete' => false,
         ],
     ];
-
 //    protected static $_eav = [
 //        'person_metadata' => [            // we use the statistics relation to store the EAV data
 //            'attribute' => 'key',        // the key column in the related table contains the attribute
 //            'value' => 'value',            // the value column in the related table contains the value
 //        ],
 //    ];
-    
+
     protected static $_many_many = [
     ];
-
     protected static $_has_one = [
     ];
-
     protected static $_belongs_to = [
     ];
 
-    public static function image($id)
-    {
+    public static function image($id) {
         $image['photo'] = \DB::select('photo')->from('person')->where('id', '=', $id)->as_object()->execute();
         return $image;
+    }
 
+    public static function menuPersons() {
+        $query = \DB::select('display', 'id')->from('persons')->order_by('display', 'desc')->as_assoc()->execute();
+        $seasons = [];
+        foreach ($query as $result) {
+            $seasons[$result['display']] = $result['display'] . " " . $result['id'];
+        }
+
+        return $seasons;
     }
 
 }
--- fuel/app/views/admin/person/index.php
+++ fuel/app/views/admin/person/index.php
@@ -2,17 +2,15 @@
 <br>
 
 <?php if ($people): ?>
-	<div class="table-responsive">
-		<table class="table table-striped">
+	<div class="w3-responsive">
+		<table class="w3-table-all">
 			<thead>
 				<tr>
-					<th>Id</th>
 					<th>First</th>
 					<th>Last</th>
 					<th>Nick</th>
+                                        <th>Display</th>
 					<th>Photo</th>
-					<th>Submitted date</th>
-					<th>Updated date</th>
 					<th></th>
 				</tr>
 			</thead>
@@ -20,13 +18,11 @@
 			<tbody>
 				<?php foreach ($people as $item): ?>
 					<tr>
-						<td><?php echo $item->id; ?></td>
 						<td><?php echo $item->first; ?></td>
 						<td><?php echo $item->last; ?></td>
 						<td><?php echo $item->nick; ?></td>
+						<td><?php echo $item->display; ?></td>
 						<td><?php echo $item->photo; ?></td>
-						<td><?php echo $item->submitted_date; ?></td>
-						<td><?php echo $item->updated_date; ?></td>
 
 						<td>
 							<?php echo Html::anchor('admin/person/view/'.$item->id, 'View'); ?> |
